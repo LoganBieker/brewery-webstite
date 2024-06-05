b574b3410ef35ab9aaf216ea37988abba909656e
LoganBieker <133885615+LoganBieker@users.noreply.github.com>
2024-06-05 13:22:16 -0400

    dev combined sql & events serverside functionality. Added additional SQL query to return all active brews. Cleaned up some error handling client side. Added OnTap page

--- react-Brewery/backend/server.js
+++ react-Brewery/backend/server.js
@@ -4,41 +4,43 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const path = require('path');
 const eventsManager = require('./eventsManager.js')
+const sqlServerConnection = require('./sqlServerConnection.js')
 app.use(cors());
 app.use(express.json());
 
-const eventsDir = path.join(__dirname, 'events');
-const m_eventMgr = new eventsManager(eventsDir);
+function main() {
+    const eventsDir = path.join(__dirname, 'events');
+    const m_eventMgr = new eventsManager(eventsDir);
 
-let eventData = null;
-initializeEvents()
-    .then(data => {
-        eventData = data;
-        console.log("Events data init and stored")
-    })
-    .catch(err => {
-        console.log("Failed to init data")
-    });
+    let eventData = null;
 
-
-
-
-async function initializeEvents() {
-    const events = await m_eventMgr.getEventObject();
-    //console.log("Server Side")
-    console.log(events);
-    return events
-}
-
-app.listen(PORT, () => {
-    console.log(`server is running on http:://localhost:${PORT}`);
-});
-
-app.get('/api/data', (req, res) => {
-    if (eventData) {
-        res.json(eventData);
-    } else {
-        res.status(503).json({ error: "Data is not yet avilble. Please try again later" })
+    async function initializeEvents() {
+        const events = await m_eventMgr.getEventObject();
+        //console.log("Server Side")
+        //console.log(events);
+        return events
     }
 
-});
+    app.listen(PORT, () => {
+        console.log(`server is running on http:://localhost:${PORT}`);
+    });
+
+    app.get('/api/data', (req, res) => {
+            initializeEvents()
+                .then(data => {
+                    eventData = data;
+                    console.log("Events data init and stored")
+                    res.json(eventData);
+                })
+                .catch(err => {
+                    console.log("Failed to init data")
+                    res.status(503).json({ error: "Data is not yet avilble. Please try again later" })
+                })
+
+    });
+    // gets brew info from database
+    sqlServerConnection.getSQLData(app);
+    //sqlServerConnection()
+}
+
+main()
