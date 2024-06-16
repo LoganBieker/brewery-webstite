fff663e421c3d6ad94e1cf3444a8c1a58b151daf
LoganBieker <133885615+LoganBieker@users.noreply.github.com>
2024-06-16 09:10:14 -0400

    Finished initial formatting on the About us section

--- react-Brewery/src/pages/TheBrewery.jsx
+++ react-Brewery/src/pages/TheBrewery.jsx
@@ -1,14 +1,18 @@
 import DropDownContainer from '@/Components/DropDownMenu/DropDownContainer'
-import '@/styles/DropDownContainer.css'
+import '@/styles/TheBrewery.css'
 export default function TheBrewery() {
 
 
     return (
         <>
             <DropDownContainer></DropDownContainer>
-            <div className='drop-down-container-text'>
-                Trash Panda Brewing is a pico brewery located in the heart of Baltimore. We’re located walking distance from the Ravens Stadiums, Orioles Stadium & the gorgeous Inner Harbor.  We are an experimental brewery and brew many types of unique beverages. We have an array of revolving Beers & Hydromel’s (a light crisp and refreshing session mead). Come by for a game night or one of our other various events in the area.
-            </div>
+            <div className='the-brewery-container'>
+                <div className='the-brewery-text'>
+                    <span className='text-blue'>Trash Panda Brewing</span> is a pico brewery located in the heart of Baltimore. We’re located walking distance from the <span className='text-purple'>Ravens Stadium</span>, <span className='text-orange'>Orioles Stadium</span> & the gorgeous Inner Harbor.
+                    We are an experimental brewery and brew many types of unique beverages. We have an array of revolving Beers & Hydromel’s (a light crisp and refreshing session mead).
+                    Come by for a game night or one of our other various events in the area.
+                </div>
+            </div >
             <div className='image-container'>
                 <img className='image' src='./public/fridge2.jpg' alt='Setup Image'></img>
                 <img className='image' src='./public/house.jpg' alt='House Imgage'></img>
